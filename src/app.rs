497933ccd2cde0f883e57aa6846b9fc5304f97e8
Dan Kirkham <Daniel.A.Kirkham@gmail.com>
2022-07-16 09:27:33 -0500

    All components now pulling from settings

--- src/app.rs
+++ src/app.rs
@@ -1,20 +1,45 @@
-use egui::*;
 use std::sync::mpsc::Receiver;
+use std::sync::{Arc, RwLock, mpsc};
+use std::thread;
 use std::time::Duration;
+
+use egui::*;
 
 use crate::plot_data::{PlotData, PlotRow, new_plot_data};
 use crate::waterfall_plot::WaterfallPlot;
+use crate::recorder::{Recorder, RecorderData};
+use crate::waterfall_processor::WaterfallProcessor;
+use crate::configuration::{Configuration, GlobalConfig};
+
 
 pub struct App {
     plot_row_rx: Receiver<PlotRow>,
     plot_data: PlotData,
+    config: GlobalConfig,
 }
 
 impl App {
-    pub fn new(_cc: &eframe::CreationContext<'_>, plot_row_rx: Receiver<PlotRow>) -> Self {
+    pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+        let (tx, rx) = mpsc::channel::<PlotRow>();
+        let (sample_tx, sample_rx) = mpsc::channel::<RecorderData>();
+
+        let config = Arc::new(RwLock::new(Configuration::default()));
+        let r_config = config.clone();
+        thread::spawn(move || {
+            let mut recorder = Recorder::new(sample_tx, r_config);
+            recorder.start();
+        });
+
+        let p_config = config.clone();
+        thread::spawn(move || {
+            let mut processor = WaterfallProcessor::new(sample_rx, tx, p_config);
+            processor.start();
+        });
+
         Self {
-            plot_row_rx,
+            plot_row_rx: rx,
             plot_data: new_plot_data(),
+            config,
         }
     }
 }
@@ -29,6 +54,10 @@ impl eframe::App for App {
         //             }
         //         });
         //     });
+        // });
+
+        // egui::SidePanel::left("waterfall_settings").show(ctx, |ui| {
+        //    ui.label("Waterfall Settings");
         // });
 
         egui::CentralPanel::default()
@@ -47,8 +76,11 @@ impl eframe::App for App {
         //     });
         // });
 
+        let fft_depth = self.config.read().unwrap().fft_depth;
+        let audio_sample_rate = self.config.read().unwrap().audio_sample_rate;
+
         ctx.request_repaint_after(std::time::Duration::from_millis(
-            4096 / 44100
+            (fft_depth / audio_sample_rate) as u64
         ));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,40 +6,16 @@ mod turbo;
 mod waterfall_plot;
 mod waterfall_processor;
 
-use std::thread;
-use std::time::Duration;
-use std::sync::{Arc, RwLock, mpsc};
-
 use app::App;
-use plot_data::{PlotRow};
-use recorder::{Recorder, RecorderData};
-use waterfall_processor::WaterfallProcessor;
-use configuration::{Configuration, GlobalConfig};
 
 fn main() {
     // Log to stdout (if you run with `RUST_LOG=debug`).
     // tracing_subscriber::fmt::init();
-    let config = Arc::new(RwLock::new(Configuration::default()));
-
-    let (tx, rx) = mpsc::channel::<PlotRow>();
-    let (sample_tx, sample_rx) = mpsc::channel::<RecorderData>();
-
-    let r_config = config.clone();
-    thread::spawn(move || {
-        let mut recorder = Recorder::new(sample_tx, r_config);
-        recorder.start();
-    });
-
-    let p_config = config.clone();
-    thread::spawn(move || {
-        let mut processor = WaterfallProcessor::new(sample_rx, tx, p_config);
-        processor.start();
-    });
 
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(
         "Waterfall",
         native_options,
-        Box::new(|cc| Box::new(App::new(cc, rx))),
+        Box::new(|cc| Box::new(App::new(cc))),
     );
 }
