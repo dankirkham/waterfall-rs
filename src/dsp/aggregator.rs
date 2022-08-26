e2840374f557903597dad0772770e1301135dc45
Dan Kirkham <Daniel.A.Kirkham@gmail.com>
2022-08-25 22:25:33 -0500

    Optimize aggregator

--- src/dsp/aggregator.rs
+++ src/dsp/aggregator.rs
@@ -1,15 +1,17 @@
+use std::collections::VecDeque;
+
 use crate::recorder::RecorderData;
 
 pub struct Aggregator {
     buffer_len: usize,
-    data: Vec<RecorderData>,
+    data: VecDeque<RecorderData>,
 }
 
 impl Aggregator {
     pub fn new(buffer_len: usize) -> Self {
         Self {
             buffer_len,
-            data: Vec::with_capacity(buffer_len)
+            data: VecDeque::with_capacity(10 * buffer_len)
         }
     }
 
@@ -22,10 +24,11 @@ impl Aggregator {
             return None;
         }
 
-        let subset = self.data[0..self.buffer_len].to_vec();
+        // let subset = self.data[0..self.buffer_len].to_vec();
+        // self.data.rotate_left(self.buffer_len);
+        // self.data.resize(self.data.len() - self.buffer_len, 0.0);
 
-        self.data.rotate_left(self.buffer_len);
-        self.data.resize(self.data.len() - self.buffer_len, 0.0);
+        let subset: Vec<RecorderData> = self.data.drain(..self.buffer_len).collect();
 
         Some(subset)
     }
