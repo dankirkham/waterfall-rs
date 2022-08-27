e591b99e8b3bcb9f46a6cc6adb434489ce4ddad2
Dan Kirkham <Daniel.A.Kirkham@gmail.com>
2022-08-26 20:17:45 -0500

    WASM?!

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,5 +1,6 @@
 use waterfall_rs::app::App;
 
+#[cfg(not(target_arch = "wasm32"))]
 fn main() {
     // Log to stdout (if you run with `RUST_LOG=debug`).
     // tracing_subscriber::fmt::init();
@@ -10,4 +11,21 @@ fn main() {
         native_options,
         Box::new(|cc| Box::new(App::new(cc))),
     );
+}
+
+#[cfg(target_arch = "wasm32")]
+fn main() {
+    // Make sure panics are logged using `console.error`.
+    console_error_panic_hook::set_once();
+
+    // Redirect tracing to console.log and friends:
+    tracing_wasm::set_as_global_default();
+
+    let web_options = eframe::WebOptions::default();
+    eframe::start_web(
+        "Waterfall",
+        web_options,
+        Box::new(|cc| Box::new(App::new(cc))),
+    )
+    .expect("failed to start eframe");
 }
