30c145c267f0c61d91d6ff07113d7d56ab52effa
Dan Kirkham <Daniel.A.Kirkham@gmail.com>
2022-08-28 21:54:04 -0500

    Add commit hash to about page

--- src/ui/about.rs
+++ src/ui/about.rs
@@ -1,3 +1,6 @@
+use std::env;
+use std::fmt;
+
 use egui::*;
 
 pub struct About;
@@ -9,6 +12,8 @@ impl About {
 
     pub fn ui(&mut self, ui: &mut egui::Ui) {
         ui.heading("waterfall-rs");
+        let version: &'static str = option_env!("WATERFALL_VERSION").unwrap_or("development");
+        ui.label(format!("Build: {version}"));
         ui.add_space(10.0);
         ui.label("A software-defined radio (SDR) tool written in Rust.");
         ui.add_space(10.0);
